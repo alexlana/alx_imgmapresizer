51c15e646193834b859b12999765be50dddf8e71
alexlana <alexlana@gmail.com>
2020-09-03 14:27:35 -0300

    2020-09-03 - 2

--- hex_imgmapresizer.js
+++ hex_imgmapresizer.js
@@ -1,33 +1,30 @@
+const hex_mapas = document.getElementsByTagName('map');
 function hex_imgmapresizer() {
-  const mapas = document.getElementsByTagName('map');
-  window.addEventListener( 'load', ()=>{
-    for ( var i=0; i<mapas.length; i++ ) {
-      var imagem = document.querySelector('img[usemap="#'+mapas[i].getAttribute('name')+'"]');
-      var r = imagem.width/imagem.naturalWidth;
-      var areas = mapas[i].getElementsByTagName('area');
-      var dcoords = new Array();
-      for ( var j=0; j<areas.length; j++ ) {
-        var coords = areas[j].getAttribute('coords').split(',');
-        dcoords[j] = coords;
-        coords = coords.map( (c)=>{return Math.round(c*1*r);} );
-        coords = coords.join(',');
-        areas[j].setAttribute('coords',coords);
-      }
-      mapas[i].setAttribute('data-coords', JSON.stringify(dcoords) );
-    }
-  } );
-  window.addEventListener( 'resize', ()=>{
-    for ( var i=0; i<mapas.length; i++ ) {
-      var imagem = document.querySelector('img[usemap="#'+mapas[i].getAttribute('name')+'"]');
-      var r = imagem.width/imagem.naturalWidth;
-      var areas = mapas[i].getElementsByTagName('area');
-      var dcoords = JSON.parse( mapas[i].getAttribute('data-coords') );
-      for ( var j=0; j<areas.length; j++ ) {
-        var coords = dcoords[j];
-        coords = coords.map( (c)=>{return Math.round(c*1*r);} );
-        coords = coords.join(',');
-        areas[j].setAttribute('coords',coords);
-      }
-    }
-  } );
+	window.addEventListener( 'load', ()=>{
+		for ( var i=0; i<hex_mapas.length; i++ ) {
+			var areas = hex_mapas[i].getElementsByTagName('area');
+			var dcoords = new Array();
+			for ( var j=0; j<areas.length; j++ ) {
+				var coords = areas[j].getAttribute('coords').split(',');
+				dcoords[j] = coords;
+			}
+			hex_mapas[i].setAttribute('data-coords', JSON.stringify(dcoords) );
+		}
+		hex_resizemap();
+	} );
+	window.addEventListener( 'resize', hex_resizemap );
+}
+function hex_resizemap () {
+	for ( var i=0; i<hex_mapas.length; i++ ) {
+	  var imagem = document.querySelector('img[usemap="#'+hex_mapas[i].getAttribute('name')+'"]');
+	  var r = imagem.width/imagem.naturalWidth;
+	  var areas = hex_mapas[i].getElementsByTagName('area');
+	  var dcoords = JSON.parse( hex_mapas[i].getAttribute('data-coords') );
+	  for ( var j=0; j<areas.length; j++ ) {
+	    var coords = dcoords[j];
+	    coords = coords.map( (c)=>{return Math.round(c*1*r);} );
+	    coords = coords.join(',');
+	    areas[j].setAttribute('coords',coords);
+	  }
+	}
 }
